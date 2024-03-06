85bb7928017aced760e0f648a0befa4127f7e7dd
Sheraz Arif <sherazarifofficial@gmail.com>
2024-03-07 00:54:25 +0500

    added a new section name community_card

---
+++ src/app/_components/community_card/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import Community from "./community";
+
+const page = () => {
+  return (
+    <div>
+      <Community />
+    </div>
+  );
+};
+
+export default page;
