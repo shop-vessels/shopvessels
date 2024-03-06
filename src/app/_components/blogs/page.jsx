5150f9356019b482a1bbd5468972f50d2779a9d9
Sheraz Arif <sherazarifofficial@gmail.com>
2024-03-07 00:52:24 +0500

    added a new section name blogs

---
+++ src/app/_components/blogs/page.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import Blog from "../blogs/blogs";
+import blogData from "../../../data/Blog.json";
+
+const page = () => {
+  return (
+    <div>
+      <div className="lg:p-12 p-4">
+        <div className="pt-16">
+          <p className="text-3xl text-center pb-10">blog posts</p>
+        </div>
+        <div className=" grid lg:grid-cols-3 grid-cols-1 gap-3">
+          {blogData.map((data, index) => (
+            <Blog
+              key={index}
+              image={data.image}
+              title={data.title}
+              description={data.explanation}
+            />
+          ))}
+        </div>
+        <div className="text-center mt-8">
+          <button className="uppercase bg-[#de9e27] px-5 py-3 rounded-sm text-white text-sm">
+            view more
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
