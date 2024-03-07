b209f5de1bd4ffb0a41f312533bb392052e22a74
Zain Ul Abideen <csezain@gmail.com>
2024-03-07 08:02:23 +0500

    Configured ShadCN UI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,5 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Hellow Zain</Button>
     </main>
-    
-    
   );
 }
