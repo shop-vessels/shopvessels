573789b2c9cb00d738fa85ade624fbe3bd4db16f
Sheraz Arif <sherazarifofficial@gmail.com>
2024-03-12 16:15:17 +0500

    added another section of contact-form

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
-import Footer from "../components/footer/page";
+import Footer from "../components/footer/footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -17,9 +17,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
-          <Navbar />
-          {children}
-          <Footer />
+        <Navbar />
+        {children}
+        <Footer />
       </body>
     </html>
   );
