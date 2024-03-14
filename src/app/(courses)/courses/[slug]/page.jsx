2606588706be72564972eba9b93983f70f2ac8a2
Zain Ul Abideen <csezain@gmail.com>
2024-03-14 16:25:36 +0500

    Added Sidebar of course page

---
+++ src/app/(courses)/courses/[slug]/page.jsx
@@ -0,0 +1,48 @@
+
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { ChevronLeft } from "lucide-react";
+import React from "react";
+import SearchBar from "./_components/SearchBar";
+import WeeksAccordion from "./_components/WeeksAccordion";
+
+const Page = () => {
+  return (
+    <div className="flex justify-center items-center h-screen relative">
+      <aside className="border-r w-full max-w-sm p-5 !h-full overflow-y-auto">
+        <CourseMetaBlock />
+        <SearchBar />
+        <WeeksAccordion />
+      </aside>
+      <main className="flex-1 flex justify-center items-center p-10  max-w-5xl mx-auto min-h-screen overflow-y-auto">
+        Content
+      </main>
+    </div>
+  );
+};
+
+const CourseMetaBlock = () => {
+  return (
+    <div className="border shadow-md rounded-md">
+      <div className="text-center bg-primary py-3">
+        <p>Logo</p>
+      </div>
+      <div className="p-2">
+        <div className="text-xs flex gap-0.5 text-foreground/60 mt-2 items-center">
+          <ChevronLeft size={14} /> <span>Go to Dashboard</span>
+        </div>
+        <h2 className="mt-2 text-xl font-medium text-foreground/80">
+          QuantumPsyche Programming (QPP): The 40-Day Blueprint for Elevated
+          Consciousness
+        </h2>
+        <div className="mt-2">
+          <Progress value={10} className="h-1 mb-2" />
+          <span>10% </span>
+          <span className="text-xs">complete</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- src/app/stories-blogs/blogs/stories/stories.jsx
+++ src/app/stories-blogs/blogs/stories/stories.jsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 
 const stories = ({ image, title, publishedAt, description }) => {
   return (
-<<<<<<< HEAD
     <section className=" mx-auto border p-5 rounded-lg cursor-pointer hover:shadow-lg hover:scale-[1.01] transition-all">
       <div className="relative aspect-video rounded-md overflow-hidden">
         <Image
@@ -15,20 +14,6 @@ const stories = ({ image, title, publishedAt, description }) => {
       </div>
       <div>
         <h2 className="text-xl font-semibold py-2">{title}</h2>
-=======
-    <section className="max-w-7xl mx-auto flex justify-center items-center flex-col">
-      <div className=" ">
-        <Image
-          src={image}
-          width={650}
-          height={400}
-          alt="Image"
-          className="w-"
-        />
-      </div>
-      <div className="md:w-full w-[90%]">
-        <p className="text-xl font-semibold py-2">{title}</p>
->>>>>>> sheraz
         <p className="py-4 text-black/45 text-sm">{publishedAt}</p>
         <p className="text-base font-normal line-clamp-3 opacity-70">
           {description}
