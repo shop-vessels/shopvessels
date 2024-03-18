aa888bbd57336887d42ce833f0eef8de5d096973
Sheraz Arif <sherazarifofficial@gmail.com>
2024-03-18 16:27:03 +0500

    added loading section

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="flex justify-center items-center flex-col md:p-16 p-10">
+      <div
+        class="w-12 h-12 rounded-full animate-spin
+  border-8 border-solid border-primary border-t-transparent mt-10"
+      ></div>
+    </div>
+  );
+}
