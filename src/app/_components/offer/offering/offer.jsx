533f3c7a3169c175b5c9a1bb811e3e1f324ff8b2
Sheraz Arif <sherazarifofficial@gmail.com>
2024-04-15 16:33:58 +0500

    added some changes

--- src/app/_components/offer/offering/offer.jsx
+++ src/app/_components/offer/offering/offer.jsx
@@ -3,6 +3,7 @@ import image from "../../../../../public/images/offer/offerimg/offer.png";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Play } from "lucide-react";
+import Link from "next/link";
 
 const offer = () => {
   return (
@@ -17,9 +18,12 @@ const offer = () => {
               alt="image"
               className="lg:rounded-none rounded-lg"
             />
-            <div className="z-10  w-20 h-20 bg-primary rounded-full flex justify-center items-center hover:scale-105 transition-all duration-300 absolute lg:top-1/3 top-1/3 lg:right-48 right-1/2">
+            <Link
+              className="z-10  w-20 h-20 bg-primary rounded-full flex justify-center items-center hover:scale-105 transition-all duration-300 absolute lg:top-1/3 top-1/3 lg:right-40 right-1/2"
+              href="/all-courses"
+            >
               <Play className="bg-transparent text-black/65 w-8 h-8 " />
-            </div>
+            </Link>
           </div>
           <div className="">
             <p className="sm:text-4xl text-2xl font-semibold">
