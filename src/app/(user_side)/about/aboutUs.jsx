2667bf3f5adf67551dc68549be69f1f55b2e51fe
Sheraz Arif <sherazarifofficial@gmail.com>
2024-05-04 13:46:04 +0500

    made changes om about page

--- src/app/(user_side)/about/aboutUs.jsx
+++ src/app/(user_side)/about/aboutUs.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import image from "../../../../public/images/about/white-vertical.png";
 import bgImage from "../../../../public/images/about/bgImage.jpg";
 import { Instagram } from "lucide-react";
-import { Facebook } from "lucide-react";
+import { Twitter } from "lucide-react";
 import { Play } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
@@ -20,19 +20,15 @@ const aboutUs = () => {
       />
       <div className=" z-10 flex flex-col lg:flex-row items-center lg:justify-around ">
         <div className="flex-1 flex items-center lg:flex-row flex-col gap-8 text-white pb-6 w-full">
-          <div className="z-10  w-20 h-20 bg-primary rounded-full flex justify-center items-center hover:scale-105 transition-all duration-300 min-w-20">
-            <Play className="bg-transparent text-black/65 w-7 h-7 " />
-          </div>
           <div className="">
             <Image
               src={image}
               responsive="true"
               alt="image"
-              className=" w-56"
+              className=" w-64"
             />
-            {/* <p className="pt-1 lg:pl-4">Yoga That Resonates</p> */}
           </div>
-          <p className=" lg:w-[570px] w-[85%] text-base font-semibold ">
+          <p className=" lg:w-[670px] w-[85%] text-base font-semibold ">
             Vessels aims to become a holistic wellness leader, merging products
             with educational insights. While our immediate offerings center
             around the Archive 00: Genesis&rsquo;s NOW Program, it&rsquo;s just
@@ -46,21 +42,15 @@ const aboutUs = () => {
           </p>
         </div>
         <div className="flex-grow flex lg:justify-end items-center h-12 flex-row gap-2">
-          <a
-            href="#"
-            className="self-center bg-white w-8 h-8 flex justify-center items-center rounded-full hover:scale-105 transition-all duration-300"
-          >
-            <Instagram className="text-black/45  " />
-          </a>
-          <a
-            href="#"
-            className="self-center bg-white w-8 h-8 flex justify-center items-center rounded-full hover:scale-105 transition-all duration-300"
-          >
-            <Facebook className="text-black/45  " />
-          </a>
+          <Link href="https://www.instagram.com/qppacademy">
+            <Instagram className="hover:scale-110 transition-all duration-400 w-7 h-7 hover:text-foreground text-white" />
+          </Link>
+          <Link href="https://twitter.com/@QPPacademy">
+            <Twitter className="hover:scale-110 transition-all duration-400 w-7 h-7 hover:text-foreground text-white" />
+          </Link>
           <Button
             asChild
-            className="uppercase border-2 text-sm tracking-widest border-white py-5 px-4 text-white self-start bg-transparent"
+            className="uppercase border-2 text-sm tracking-widest border-white py-5 px-4 text-white self-start bg-transparent hover:border-primary"
             href="our-values"
           >
             <Link href="/our-values">learn more</Link>
