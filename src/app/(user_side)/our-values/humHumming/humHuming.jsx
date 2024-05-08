95e3a2de201a81fe4669ee74c0915db2d1c459cd
Sheraz Arif <sherazarifofficial@gmail.com>
2024-05-09 00:25:49 +0500

    optimized images has been changed

--- src/app/(user_side)/our-values/humHumming/humHuming.jsx
+++ src/app/(user_side)/our-values/humHumming/humHuming.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import founder from "../../../../../public/images/ourValues/founder.png";
+import founder from "../../../../../public/images/ourValues/founder.jpg";
 
 const humHuming = () => {
   return (
@@ -13,18 +13,24 @@ const humHuming = () => {
           <p className="md:text-lg mt-4">
             Vessels embodies what I needed during my battle with anxiety and
             deep depression years ago—a brand that understands your process to
-            wholeness. Through self-inquiry, I&apos;ve realized the profound power of
-            inner healing and realized, in the universal truth of oneness. I&apos;m
-            deeply passionate about sharing the NOW Method with the world.{" "}
-            <br /> Now, as a Meditation teacher, Breathwork Facilitator and
-            Integration Coach, I aim to hold space for others as they discover
-            this truth within themselves. Acknowledging the self, plant
-            medicine, meditation, fasting and breathwork have been pivotal in my
-            personal journey, and I believe in their potential for global
+            wholeness. Through self-inquiry, I&apos;ve realized the profound
+            power of inner healing and realized, in the universal truth of
+            oneness. I&apos;m deeply passionate about sharing the NOW Method
+            with the world. <br /> Now, as a Meditation teacher, Breathwork
+            Facilitator and Integration Coach, I aim to hold space for others as
+            they discover this truth within themselves. Acknowledging the self,
+            plant medicine, meditation, fasting and breathwork have been pivotal
+            in my personal journey, and I believe in their potential for global
             healing.
           </p>
         </div>
-        <Image src={founder} width={520} height={520} alt="image" />
+        <Image
+          src={founder}
+          width={520}
+          height={520}
+          alt="image"
+          className="mt-5 rounded-md"
+        />
       </div>
     </div>
   );
